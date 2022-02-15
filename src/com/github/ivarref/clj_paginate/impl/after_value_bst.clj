ba837242fcfc542b131973a8b9fb84fd8baccc60
Ivar Refsdal <ivar.refsdal@nsd.no>
2022-02-15 12:05:38 +0100

    Clean up / remove old code

--- src/com/github/ivarref/clj_paginate/impl/after_value_bst.clj
+++
@@ -1,1 +0,0 @@
-(ns com.github.ivarref.clj-paginate.impl.after-value-bst)
--- src/com/github/ivarref/clj_paginate/impl/utils.clj
+++ src/com/github/ivarref/clj_paginate/impl/utils.clj
@@ -1,22 +1,7 @@
 (ns com.github.ivarref.clj-paginate.impl.utils
   (:require [clojure.edn :as edn]
-            [com.github.ivarref.clj-paginate.impl.bst :as bst]
             [clojure.string :as str])
-  (:import (java.util UUID)
-           (java.io StringWriter Writer)))
-
-
-(defonce version (delay (str (UUID/randomUUID))))
-
-
-(defn balanced-tree
-  [v opts]
-  (let [srt-by (get opts :sort-by)
-        v (vec (sort-by (apply juxt srt-by) v))]
-    {:root         (bst/balanced-tree v)
-     :input-vector v
-     :version      (str (get opts :version @version))
-     :opts         {:sort-by srt-by}}))
+  (:import (java.io StringWriter Writer)))
 
 
 (defn maybe-decode-cursor [cursor]
@@ -27,11 +12,6 @@
       (edn/read-string cursor))))
 
 
-(defn old-cursor? [cursor version]
-  (when (map? cursor)
-    (not= version (get cursor :version))))
-
-
 (defn get-cursor [opts]
   (or
     (let [cursor (or (get opts :after) (get opts :before))]
@@ -40,23 +20,6 @@
                  (str/starts-with? cursor "{"))
         (edn/read-string cursor)))
     {}))
-
-
-(defonce constantly-true (constantly true))
-
-
-(defn get-total-count [root keep? id decoded-cursor old-count]
-  (when root
-    (let [v (.-v root)]
-      (loop [i 0
-             cnt 0]
-        (if (= i (count v))
-          cnt
-          (recur (inc i)
-                 (+ cnt
-                    (if (keep? (nth v i))
-                      1
-                      0))))))))
 
 
 (defn cursor-pre [cursor]
